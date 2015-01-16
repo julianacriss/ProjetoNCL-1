3b9f38059b874b29705362298fa01c682c070544
Juliana Cristina de Andrade <julianacristina.ti@gmail.com>
2015-01-16 14:03:10 -0200

    Versão 3
    Pequenas alterações.

--- main.lua
+++ main.lua
@@ -20,7 +20,6 @@ local function sinal1()
     local path = "/misc/ncl30/mpeg-u.xml"
     local arquivo = io.open(path, "r")
     if arquivo then
-      --print("--> ARQUIVO EXISTE!")
       io.input(arquivo)
       local content = io.read()
       if (content ~= nil) then
@@ -35,28 +34,12 @@ local function sinal1()
             evt2.action = 'start'; event.post(evt2)
             evt2.action = 'stop' ; event.post(evt2)
           end
-          print("antes do close")
         io.close(arquivo)
-      print("depois do close e antes do remove")
       os.remove(path)
-      print("depois do remove")
-      end -- if content
-      
+      end -- if content    
     end --if arquivo
-    
   end -- while
 end -- function
 
-local function sinal2()
-  local evt = {
-    class = 'ncl',
-    type  = 'attribution',
-    name  = 'sinal2',
-    value = 2,
-  }
-  evt.action = 'start'; event.post(evt)
-  evt.action = 'stop' ; event.post(evt)
-end
 
-event.timer(2000, sinal1)
-event.timer(5000, sinal2)
+event.timer(0, sinal1)
